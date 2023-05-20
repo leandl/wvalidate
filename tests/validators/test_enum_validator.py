f6df092739e4dd1dd0bd765bc4e1aefe1e4846df
leandl <leandrocrispim891@gmail.com>
2023-05-20 18:18:11 -0300

    + Added EnumValidator

---
+++ tests/validators/test_enum_validator.py
@@ -0,0 +1,47 @@
+import pytest
+
+from wvalidate import Validator, ValidatorReturn, ValidatorError
+from wvalidate.validators.enum_validator import EnumValidator, EnumValidatorException
+
+
+def test_enum_validator_is_instance_validator():
+	assert issubclass(EnumValidator, Validator) == True
+  
+def test_enum_validator_without_a_list_of_options_2_items():           
+	with pytest.raises(EnumValidatorException):
+		EnumValidator()
+	
+	with pytest.raises(EnumValidatorException):
+		EnumValidator([])
+
+	TEST_WITHOUT_LIST_WITH_2_ITEMS = ["TEST"]
+	with pytest.raises(EnumValidatorException):
+		EnumValidator(TEST_WITHOUT_LIST_WITH_2_ITEMS)    
+
+	
+def test_enum_validator_not_validate():
+	TESTS = [
+		(["ACTIVE", "DELETED"], "OK"),
+		([2, 3, 5], 7),
+		([False, True], None),
+		([None, "DELETED", 1], False),
+	]
+
+	for test, data in TESTS:
+		validator_return = ValidatorReturn(False, ValidatorError(f"is not among the options. {test}"))
+		assert EnumValidator(test).is_valid(data) == validator_return
+
+
+def test_enum_validator_validate():
+	TESTS = [
+		(["ACTIVE", "DELETED"], "ACTIVE"),
+		(["ACTIVE", "DELETED"], "DELETED"),
+		([2, 3, 5], 2),
+		([False, True, None], None),
+		([None, "DELETED", 1], 1),
+		([41, None, 15, "DELETED", 31], 15),
+	]
+
+	for test, data in TESTS:
+		validator_return = ValidatorReturn(True)
+		assert EnumValidator(test).is_valid(data) == validator_return
---
+++ wvalidate/validators/enum_validator.py
@@ -0,0 +1,23 @@
+from __future__ import annotations
+from typing import List, Any
+
+from .. import Validator, ValidatorError, ValidatorReturn
+
+
+class EnumValidatorException(Exception):
+	pass
+
+class EnumValidator(Validator):
+    
+	def __init__(self, options: List[Any] = None) -> None:
+		if options == None or not isinstance(options, list) or len(options) < 2:
+			raise EnumValidatorException("Expected a list of options with at least 2 items")
+
+		super().__init__()
+		self.__options = options
+  
+	def is_valid(self, data) -> ValidatorReturn:
+		if data not in self.__options:
+			return ValidatorReturn(False, ValidatorError(f"is not among the options. {self.__options}"))
+		
+		return ValidatorReturn(True)
